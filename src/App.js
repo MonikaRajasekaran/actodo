14c183c04f17ea9defe7ed97ed086441052af0a9
Monika Rajasekaran <monikarajasekaran16395@gmail.com>
2024-01-02 09:33:09 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/header";
+import Card from "./components/card";
+import AddToDoForm from "./components/AddTodoForm";
+import TodoContainer  from "./components/TodoContainer";
+import { BrowserRouter , Routes , Route } from "react-router-dom";
+import Login from "./pages/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Signup from "./pages/Signup";
+import Landing from "./pages/Landing";
+import { useState } from "react";
+
+
+function App()
+{
+
+  const [users,setusers] = useState(
+    [
+        {
+            username:"john",
+            password:"123"
+        },
+
+        {
+          username:"abc",
+          password:"123"
+        }
+    ]
+)
+
+  return(
+    <div>
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Login users={users} setusers={setusers}/>}></Route>
+    <Route path='/signup' element={<Signup users={users} setusers={setusers}/>}></Route>
+    <Route path='/landing' element={<Landing/>}></Route>
+    </Routes>
+    </BrowserRouter>
+  </div>
+  )
 }
 
 export default App;
